0d69804c300aa961c6b6e01760dbb088add14a3a
Giorgio Bellisario <72039923+Bellisario@users.noreply.github.com>
2022-10-01 18:53:44 +0200

    Code refactoring
    Formatted all the JS code, uniformed JS code with single-quote and Python code with double quote, uniformed JS server to all arrow functions and server routes converted (from if) to switch statement for better code readability (also added a 404 response as default)

--- server.js
+++ server.js
@@ -2,63 +2,71 @@ const PORT = 8080;
 const http = require('http');
 const fs = require('fs');
 const WsHixie = require('ws-plus-hixie');
-filewatcher = require('filewatcher');
+const filewatcher = require('filewatcher');
 
 const server = http.createServer((request, response) => {
     url = request.url;
 
-    if(url.indexOf("?") > 0)
-        url = url.substring(0, url.indexOf("?"));
+    if (url.indexOf('?') > 0)
+        url = url.substring(0, url.indexOf('?'));
 
-    console.log("URL: " + url);
+    console.log('URL: ' + url);
 
-    if(!fs.existsSync('.' + url)) {
-        console.log("ERROR: NOT FOUND: " + url);
+    if (!fs.existsSync('.' + url)) {
+        console.log('ERROR: NOT FOUND: ' + url);
         return;
     }
 
-
-
-    if (url === '/' || url === '/index.html') {
-        response.setHeader('Content-Type', 'text/html');
-        fs.createReadStream('index.html').pipe(response);
-    } else if (url === '/client.js') {
-        response.setHeader('Content-Type', 'application/javascript');
-        fs.createReadStream('client.js').pipe(response);
-    } else if (url === '/gcal.png') {
-        response.setHeader('Content-Type', 'image/png');
-        fs.createReadStream('gcal.png').pipe(response);
-    } else if (url === '/screenshot.png') {
-        response.setHeader('Content-Type', 'image/png');
-        fs.createReadStream('screenshot.png').pipe(response);
+    switch (url) {
+        case '/':
+        case '/index.html':
+            response.setHeader('Content-Type', 'text/html');
+            fs.createReadStream('index.html').pipe(response);
+            break;
+        case '/client.js':
+            response.setHeader('Content-Type', 'application/javascript');
+            fs.createReadStream('client.js').pipe(response);
+            break;
+        case '/gcal.png':
+            response.setHeader('Content-Type', 'image/png');
+            fs.createReadStream('gcal.png').pipe(response);
+            break;
+        case '/screenshot.png':
+            response.setHeader('Content-Type', 'image/png');
+            fs.createReadStream('screenshot.png').pipe(response);
+            break;
+        default:
+            response.statusCode = 404;
+            response.write('Not found');
+            response.end();
     }
 });
 
 const wsBridge = new WsHixie(server);
 
-const sendMessage = function(scope, message) {
-    const json = JSON.stringify({ msj : message, sc : scope});
+const sendMessage = (scope, message) => {
+    const json = JSON.stringify({ msj: message, sc: scope });
     wsBridge.send(json);
-    console.log("message sent:" + json);
+    console.log('message sent:' + json);
 }
-server.on("error", error => {
-    if (error.code === "EADDRINUSE")
-	console.log("ERROR: Port already in use");
+server.on('error', error => {
+    if (error.code === 'EADDRINUSE')
+        console.log('ERROR: Port already in use');
     else
-	console.log(error);
+        console.log(error);
 });
 
-server.on("request", request => {
-    console.log("New request");
+server.on('request', () => {
+    console.log('New request');
 });
 
 
 // Start watching directory..
 const watcher = filewatcher();
-watchFiles = ["./screenshot.png", "./gcal.png"];
-for(const url of watchFiles){
-    if(!fs.existsSync(url)) {
-        console.log(url, "NOT FOUND, creating... ");
+watchFiles = ['./screenshot.png', './gcal.png'];
+for (const url of watchFiles) {
+    if (!fs.existsSync(url)) {
+        console.log(url, 'NOT FOUND, creating... ');
         fs.closeSync(fs.openSync(url, 'w'));
     }
     watcher.add(url);
@@ -66,15 +74,15 @@ for(const url of watchFiles){
 
 var last = Date.now();
 
-watcher.on("change", function(filename, stat) {
-    if(stat.blocks < 1 || stat.size < 1) return;
+watcher.on('change', (filename, stat) => {
+    if (stat.blocks < 1 || stat.size < 1) return;
     var now = Date.now();
-    if(now-last < 1000) return;	    // file watcher flood protection
-    console.log("onChange:", filename, stat);
-    sendMessage("bg", filename.substring(2));
+    if (now - last < 1000) return;	    // file watcher flood protection
+    console.log('onChange:', filename, stat);
+    sendMessage('bg', filename.substring(2));
     last = now;
 });
 
 server.listen(PORT);
 
-console.log("Listening on port " + PORT);
+console.log('Listening on port ' + PORT);
