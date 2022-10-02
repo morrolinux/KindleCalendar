b08f0632c4fb410fe8666eb9a98d24bb75a63b12
Mirco Ish <mirco.df@live.it>
2022-10-02 11:06:48 +0200

    Client - minor code refactoring

--- client.js
+++ client.js
@@ -2,11 +2,10 @@ const body = document.body;
 
 const COLOR_OK = '#1f7f39';
 const COLOR_ERROR = '#c43131';
-
+const fileNames = ['gcal.png', 'screenshot.png'];
 
 var active = false;
 var currentView = 0;
-var fileNames = ['gcal.png', 'screenshot.png'];
 
 
 const setMessage = function (message) {
@@ -14,7 +13,7 @@ const setMessage = function (message) {
 };
 
 const setBG = function (fileName) {
-	now = new Date().getTime();
+	const now = new Date().getTime();
 	body.style.backgroundImage = 'url("http://' + window.location.hostname + ':8080/' + fileName + '?' + now + '")';
 };
 
@@ -25,32 +24,37 @@ function cycleCurrentView() {
 
 
 const startListener = function () {
-	if (active)
+	if (active) {
 		return;
-	active = true;
-	wsConn = new WebSocket('ws://' + window.location.hostname + ':8080/');
+	}
 
+	active = true;
+	const wsConn = new WebSocket('ws://' + window.location.hostname + ':8080/');
 
 	wsConn.onopen = function () {
 		setMessage('Active', COLOR_OK);
 	}
+
 	wsConn.onclose = function (e) {
 		setMessage('Disconn. ' + JSON.stringify(e), COLOR_ERROR);
 		active = false;
 	}
+
 	wsConn.onerror = function (e, code) {
 		setMessage('ERROR ' + code + e, COLOR_ERROR);
 	}
+
 	wsConn.onmessage = function (message) {
-		// console.log('onMessage', message);
 		try {
-			var data = JSON.parse(message.data);
+			const data = JSON.parse(message.data);
 			setMessage(data.msj);
 
 			if (data.sc === 'bg') {
 				// pinned content takes over calendar refresh until page reload
-				if (data.msj === 'gcal.png' && fileNames[currentView] !== data.msj)
+				if (data.msj === 'gcal.png' && fileNames[currentView] !== data.msj) {
 					return;
+				}
+
 				currentView = fileNames.indexOf(data.msj);
 				setBG(fileNames[currentView]);
 			}
