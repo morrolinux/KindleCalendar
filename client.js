5a39022a4b5d715a77dc23a6a5bd6b40eed05c77
Alessandro Maule <71499553+LightYagami28@users.noreply.github.com>
2023-10-17 18:14:12 +0200

    Update client.js

--- client.js
+++ client.js
@@ -4,70 +4,69 @@ const COLOR_OK = '#1f7f39';
 const COLOR_ERROR = '#c43131';
 const fileNames = ['gcal.png', 'screenshot.png'];
 
-var active = false;
-var currentView = 0;
+let active = false;
+let currentView = 0;
 
-
-const setMessage = function (message) {
-	body.innerHTML = message;
+const setMessage = (message, color) => {
+  body.innerHTML = message;
+  if (color) {
+    body.style.color = color;
+  }
 };
 
-const setBG = function (fileName) {
-	const now = new Date().getTime();
-	body.style.backgroundImage = 'url("http://' + window.location.hostname + ':8080/' + fileName + '?' + now + '")';
+const setBG = (fileName) => {
+  const now = new Date().getTime();
+  body.style.backgroundImage = `url("http://${window.location.hostname}:8080/${fileName}?${now}")`;
 };
 
-function cycleCurrentView() {
-	currentView = (currentView + 1) % fileNames.length;
-	setBG(fileNames[currentView]);
+const cycleCurrentView = () => {
+  currentView = (currentView + 1) % fileNames.length;
+  setBG(fileNames[currentView]);
 };
 
+const startListener = () => {
+  if (active) {
+    return;
+  }
 
-const startListener = function () {
-	if (active) {
-		return;
-	}
+  active = true;
+  const wsConn = new WebSocket(`ws://${window.location.hostname}:8080/`);
 
-	active = true;
-	const wsConn = new WebSocket('ws://' + window.location.hostname + ':8080/');
+  wsConn.onopen = () => {
+    setMessage('Active', COLOR_OK);
+  };
 
-	wsConn.onopen = function () {
-		setMessage('Active', COLOR_OK);
-	}
+  wsConn.onclose = (e) => {
+    setMessage(`Disconn. ${JSON.stringify(e)}`, COLOR_ERROR);
+    active = false;
+  };
 
-	wsConn.onclose = function (e) {
-		setMessage('Disconn. ' + JSON.stringify(e), COLOR_ERROR);
-		active = false;
-	}
+  wsConn.onerror = (e) => {
+    setMessage(`ERROR ${e.code} ${e}`, COLOR_ERROR);
+  };
 
-	wsConn.onerror = function (e, code) {
-		setMessage('ERROR ' + code + e, COLOR_ERROR);
-	}
+  wsConn.onmessage = (message) => {
+    try {
+      const data = JSON.parse(message.data);
+      setMessage(data.msj);
 
-	wsConn.onmessage = function (message) {
-		try {
-			const data = JSON.parse(message.data);
-			setMessage(data.msj);
+      if (data.sc === 'bg') {
+        if (data.msj === 'gcal.png' && fileNames[currentView] !== data.msj) {
+          return;
+        }
 
-			if (data.sc === 'bg') {
-				// pinned content takes over calendar refresh until page reload
-				if (data.msj === 'gcal.png' && fileNames[currentView] !== data.msj) {
-					return;
-				}
-
-				currentView = fileNames.indexOf(data.msj);
-				setBG(fileNames[currentView]);
-			}
-		} catch (e) {
-			setMessage('MSG ERROR: ' + e, COLOR_ERROR);
-		}
-	}
+        currentView = fileNames.indexOf(data.msj);
+        setBG(fileNames[currentView]);
+      }
+    } catch (e) {
+      setMessage(`MSG ERROR: ${e}`, COLOR_ERROR);
+    }
+  };
 };
-
 
 // TAP to change view
-body.onclick = function () {
-	cycleCurrentView();
+body.onclick = () => {
+  cycleCurrentView();
 };
 
 startListener();
